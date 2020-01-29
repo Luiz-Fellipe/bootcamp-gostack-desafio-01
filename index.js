0fc204f2fa040c822986d0e2e52b23aff57b0434
Luiz-Fellipe <devluizfellipe@gmail.com>
2020-01-29 19:47:49 -0300

    criando rotas

--- index.js
+++ index.js
@@ -4,10 +4,55 @@ const server = express();
 
 server.use(express.json());
 
-server.get('/',(req, res) => {
-  return res.json({
-    message: 'Ola Mundo'
-  });
+const projects = [];
+
+server.get('/projects',(req, res) => {
+  return res.json(projects);
+})
+
+server.post('/projects', (req, res) => {
+  const { id, title } = req.body;
+
+  projects.push({
+    id,
+    title,
+    tasks: []
+  })
+  return res.json(projects);
+});
+
+
+server.put('/projects/:id', (req, res) => {
+  
+  const { title } = req.body;
+  const { id } = req.params;
+
+  const index = projects.findIndex(project => project.id == id);
+  projects[index].title = title;
+  
+  return res.json(projects);
+})
+
+server.delete('/projects/:id', (req,res) => {
+  const { id } = req.params;
+
+  const index = projects.findIndex(project => project.id == id);
+ 
+  projects.splice(index, 1);
+  
+  return res.json(projects);
+})
+
+
+server.post('/projects/:id/tasks', (req,res) => {
+  const {id} = req.params;
+  const {title} = req.body;
+
+  const index = projects.findIndex(project => project.id == id);
+
+  projects[index].tasks.push(title);
+
+  return res.json(projects);
 })
 
 server.listen(3000);
